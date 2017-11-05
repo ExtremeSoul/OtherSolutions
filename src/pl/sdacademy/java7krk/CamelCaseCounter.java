e6c18322451c3fb4603a6ac0015c189e57d23b60
ExtremeSoul <clegutko@gmail.com>
2017-11-05 15:54:42 +0100

    FINISCH CHANGE

--- src/pl/sdacademy/java7krk/CamelCaseCounter.java
+++ src/pl/sdacademy/java7krk/CamelCaseCounter.java
@@ -12,9 +12,17 @@ public class CamelCaseCounter {
         String camel = scanner.nextLine();
         while (!camel.isEmpty()) {
             int counter = 0;
+            if (camel.isEmpty() || Character.isUpperCase(camel.charAt(0))){
+                System.out.println("THIS IS NOT CAMEL CASE");
+                return;
+            }
             for (int i = 0; i < camel.length(); i++) {
-                if (i==0) {
+                if (i==0){
                     counter++;
+                }
+                if (Character.isDigit(camel.charAt(i))){
+                    System.out.println("WORD SHOULD NOT CONTAIN DIGITS");
+                    return;
                 }
                 if (Character.isUpperCase(camel.charAt(i))) {
                     counter++;
